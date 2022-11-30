b7b9d59be12e6d92ba721e756b6ecec8b7ac8e23
AmirHossein JafariPanah <am.amir.husein@gmail.com>
2022-11-30 04:42:02 +0330

    Finish

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import { Loader } from '@react-three/drei';
 import Experience from './Experience';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Canvas
       camera={{
-        position: [2, 2, 3],
+        position: [-2, 1.5, 3],
       }}
     >
       <Experience />
     </Canvas>
+    <Loader />
   </React.StrictMode>
 );
